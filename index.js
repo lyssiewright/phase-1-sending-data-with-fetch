37375e0f2e8e9d0b536a35a92d77e5957ccd10bb
Lyssie <lyssiewright.15@gmail.com>
2022-02-13 08:58:27 -0600

    commit tree ef228d4ed219ac36845ff879e9f551353a10d9a5

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your code here
+// function submitdata(name, email){
+//     fetch("http://localhost:3000/users", {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json",
+//     },
+//         body: JSON.stringify(name, email),
+//     })
+//     .then(response => response.json())
+//     .then(object =>  {
+//       console.log(object);
+//     })
+//     return fetch();}
+
+//     submitdata('bill', 'bill@gmail.com'); 
+
+
+
+      // method: "POST" is missing from the object below
+       const formData = {
+         name: "Byron",
+         email: "bill@gmail.com",
+       };
+      function submitData(formData){
+      fetch("http://localhost:3000/users", {
+         method: 'POST',
+         headers: {
+         "Content-Type": "application/json",
+         "Accept": "application/json",
+         },
+         body: JSON.stringify(formData),
+         })
+         .then(function (response) {
+           return response.json();
+         })
+         .then(function (object) {
+          console.log(object);
+        })
+        .catch(function (error) {
+          alert("Bad things!");
+          console.log(error.message);
+        });
+     }
+
+        
+
+        submitData(formData)
